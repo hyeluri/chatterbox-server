b26149441442f994e061f6c60ae3ad25f67fcf11
Hack Reactor Students <github@hackreactor.com>
2014-03-18 17:30:01 -0700

    express working for /classes/messages route and remote db on mongohq

--- server/basic-server.js
+++ server/basic-server.js
@@ -4,6 +4,10 @@ var http = require("http");
 var path = require('path');
 var handleRequest = require('./request-handler.js').handler;
 
+var databaseUrl = "chatterbox:mrchattersworth@oceanic.mongohq.com:10017/norman_harry"; // "username:password@example.com/mydb"
+var collections = ["messages"];
+var db = require("mongojs").connect(databaseUrl, collections);
+
 var app = express();
 
 var defaultCorsHeaders = {
@@ -28,105 +32,138 @@ app.all("*", function(req, res, next){
   next();
 });
 
-//app.get('/classes/:id', function())
 
 ////////////////////////////////////
 //everything from request-handler
 ////////////////////////////////////
 
-var storage = [];
-var messageStorage = [];
+app.storage = [];
+app.messageStorage = [];
 
-var handleRequest = function(request, response) {
+app.get('/classes/messages',function(req,res){
+   // res.send(200, {"results": app.messageStorage});
+    db.messages.find({}).sort({createdAt:-1}, function(err,msgResults){
+      if(!err){
+        res.send({results: msgResults});
+      }else{
+        console.log(err);
+        res.send(500,"error occurred");
+      }
+    });
+});
+
+app.post('/classes/messages',function(req,res){
+    //app.messageStorage.push(req.body);
+    req.body.createdAt = new Date();
+    db.messages.save(req.body,function(err,result){
+      if(!err){
+        res.send(201,"posted");
+      }else{
+        console.log(err);
+        res.send(500,"error occurred");
+      }
+    });
+});
+/*
+app.get('/classes/:roomname',function(req,res,id){
+    res.send(200, {"results": app.messageStorage});
+});
+
+app.post('/classes/:roomname',function(req,res,id){
+    app.messageStorage.push(req.body);
+    res.send(201,"posted");
+});
+
+//var handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
 
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
-  console.log("Serving request type " + request.method + " for url " + request.url);
+//   console.log("Serving request type " + request.method + " for url " + request.url);
 
-  var statusCode = 200;
+//   var statusCode = 200;
 
-  /* Without this line, this server wouldn't work. See the note
-   * below about CORS. */
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = "text/plain";
+//   /* Without this line, this server wouldn't work. See the note
+//    * below about CORS. */
+//   var headers = defaultCorsHeaders;
+//   headers['Content-Type'] = "text/plain";
 
-  if(request.method === "OPTIONS"){
-    response.writeHead(statusCode,headers);
-    response.end("ALLOW: GET POST OPTIONS");
-  }
+//   if(request.method === "OPTIONS"){
+//     response.writeHead(statusCode,headers);
+//     response.end("ALLOW: GET POST OPTIONS");
+//   }
 
-  if(request.url === '/classes/messages'){
-    messages(request,response,headers);
-  }
+//   if(request.url === '/classes/messages'){
+//     messages(request,response,headers);
+//   }
 
-  if(request.url.match(/\/classes\/room[0-9]*\/?/)){
-    //app.get('/classes/room:id', function(req, res){});
-    room(request,response,headers);
-  }else{
+//   if(request.url.match(/\/classes\/room[0-9]*\/?/)){
+//     //app.get('/classes/room:id', function(req, res){});
+//     room(request,response,headers);
+//   }else{
 
-    statusCode = 404;
-    /* .writeHead() tells our server what HTTP status code to send back */
-    response.writeHead(statusCode, headers);
+//     statusCode = 404;
+//     /* .writeHead() tells our server what HTTP status code to send back */
+//     response.writeHead(statusCode, headers);
 
-    /* Make sure to always call response.end() - Node will not send
-     * anything back to the client until you do. The string you pass to
-     * response.end() will be the body of the response - i.e. what shows
-     * up in the browser.*/
+//     /* Make sure to always call response.end() - Node will not send
+//      * anything back to the client until you do. The string you pass to
+//      * response.end() will be the body of the response - i.e. what shows
+//      * up in the browser.*/
 
-    response.end("Request failed");
-  }
-};
+//     response.end("Request failed");
+//   }
+// };
 
-var messages = function(request,response,headers){
+// var messages = function(request,response,headers){
 
-  if(request.method === "GET"){
-    response.writeHead(200,headers);
-    //response.write();
-    var obj = JSON.stringify({
-      "results": messageStorage
-    });
-    response.end(obj);
-  }
-  if(request.method === "POST"){
-    var tempStore = "";
-    request.on('data', function(data){
-      tempStore += data;
-    });
-    request.on('end', function(){
-      messageStorage.push(JSON.parse(tempStore));
-    });
-    response.writeHead(201,headers);
-    response.end("posted");
-  }
+//   if(request.method === "GET"){
+//     response.writeHead(200,headers);
+//     //response.write();
+//     var obj = JSON.stringify({
+//       "results": messageStorage
+//     });
+//     response.end(obj);
+//   }
+//   if(request.method === "POST"){
+//     var tempStore = "";
+//     request.on('data', function(data){
+//       tempStore += data;
+//     });
+//     request.on('end', function(){
+//       messageStorage.push(JSON.parse(tempStore));
+//     });
+//     response.writeHead(201,headers);
+//     response.end("posted");
+//   }
 
-};
+// };
 
-var room = function(request,response,headers){
+// var room = function(request,response,headers){
 
-  if(request.method === "GET"){
-    response.writeHead(200,headers);
-    //response.write();
-    var obj = JSON.stringify({
-      "results": storage
-    });
-    response.end(obj);
-  }
-  if(request.method === "POST"){
-    var tempStore = "";
-    request.on('data', function(data){
-      tempStore += data;
-    });
-    request.on('end', function(){
-      storage.push(JSON.parse(tempStore));
-    });
-    response.writeHead(201,headers);
-    response.end("posted");
-  }
+//   if(request.method === "GET"){
+//     response.writeHead(200,headers);
+//     //response.write();
+//     var obj = JSON.stringify({
+//       "results": storage
+//     });
+//     response.end(obj);
+//   }
+//   if(request.method === "POST"){
+//     var tempStore = "";
+//     request.on('data', function(data){
+//       tempStore += data;
+//     });
+//     request.on('end', function(){
+//       storage.push(JSON.parse(tempStore));
+//     });
+//     response.writeHead(201,headers);
+//     response.end("posted");
+//   }
 
-};
+// };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
  * This CRUCIAL code allows this server to talk to websites that
