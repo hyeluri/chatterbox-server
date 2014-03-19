ebd1888a68ae375b89d64d534359da16ada88cdd
Hack Reactor Students <github@hackreactor.com>
2014-03-18 19:16:43 -0700

    ready to deploy to azure

--- server/basic-server.js
+++ server/basic-server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var http = require("http");
 var path = require('path');
 var handleRequest = require('./request-handler.js').handler;
+var fs = require('fs');
 
 var databaseUrl = "chatterbox:mrchattersworth@oceanic.mongohq.com:10017/norman_harry"; // "username:password@example.com/mydb"
 var collections = ["messages"];
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.set('port', process.env.PORT || 3000);
 app.set('ip', "127.0.0.1");
 
+
 var server = http.createServer(app);
 console.log("Listening on http://" + app.get('ip') + ":" + app.get('port'));
 server.listen(app.get('port'), app.get('ip'));
@@ -31,14 +33,6 @@ app.all("*", function(req, res, next){
   res.set(defaultCorsHeaders);
   next();
 });
-
-
-////////////////////////////////////
-//everything from request-handler
-////////////////////////////////////
-
-app.storage = [];
-app.messageStorage = [];
 
 app.get('/classes/messages',function(req,res){
    // res.send(200, {"results": app.messageStorage});
@@ -64,6 +58,26 @@ app.post('/classes/messages',function(req,res){
       }
     });
 });
+
+app.use(function(req, res, next){
+  fs.readFile(__dirname + "/404.html", {encoding: "utf-8"}, function(err, html){
+    if(!!err){
+      console.log(err);
+    }
+    res.status(404).send(html);
+  });
+});
+
+// app.all("/*", function(req, res, next){
+//   res.status(404);
+//   next();
+//   //res.send('what sonnnn???', 404);
+// });
+// app.all("/*", express.static(path.join(__dirname, '404.html')));
+
+////////////////////////////////////
+//everything from request-handler
+////////////////////////////////////
 /*
 app.get('/classes/:roomname',function(req,res,id){
     res.send(200, {"results": app.messageStorage});
@@ -75,20 +89,7 @@ app.post('/classes/:roomname',function(req,res,id){
 });
 
 //var handleRequest = function(request, response) {
-  /* the 'request' argument comes from nodes http module. It includes info about the
-  request - such as what URL the browser is requesting. */
 
-  /* Documentation for both request and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
-
-//   console.log("Serving request type " + request.method + " for url " + request.url);
-
-//   var statusCode = 200;
-
-//   /* Without this line, this server wouldn't work. See the note
-//    * below about CORS. */
-//   var headers = defaultCorsHeaders;
-//   headers['Content-Type'] = "text/plain";
 
 //   if(request.method === "OPTIONS"){
 //     response.writeHead(statusCode,headers);
